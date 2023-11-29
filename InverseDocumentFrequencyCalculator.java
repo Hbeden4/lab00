1b0bfcbbe56d389219696a642434967e19bc6312
Hbeden4 <115094079+Hbeden4@users.noreply.github.com>
2023-11-29 12:51:46 -0500

    Add files via upload

--- InverseDocumentFrequencyCalculator.java
+++ InverseDocumentFrequencyCalculator.java
@@ -17,6 +17,7 @@ public class InverseDocumentFrequencyCalculator implements IInverseFreq {
             // Calculate the Inverse Document Frequency (IDF) Values here
             // You might need to introduce other methods and additional variables
             
+           System.arraycopy(songLyrics, 0, result, 0, 0);
             // IDF for term X is calculated as ln of (N-n(X)+0.5)/(n(X)+0.5) plus 
             // where N is total number of songs
             // and n(X) is the number of songs having term X
